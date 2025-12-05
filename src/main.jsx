4cab5efd38725480aa5bd09e5167823c80a1f4f2
Afroz-215 <afrozj215@gmail.com>
2025-12-05 22:40:20 +0530

    website

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from './App'
 import Ace from './Ace'
 
-// Check URL keyword
-const route = window.location.hash.replace("#/", "");
-
-let PageToRender;
-if (route === "web2") {
-  PageToRender = Ace;
-} else {
-  PageToRender = App; // default web1
-}
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/ace", element: <Ace /> }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PageToRender />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
